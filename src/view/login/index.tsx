9c78ad1196f917b8bc41d4c091d99458dbe7fd0f
aleclofabbro <agiansanti@uniquid.com>
2018-01-24 00:03:52 +0100

    login view cleanup

--- src/view/login/index.tsx
+++ src/view/login/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import HeaderNotLogged from './headerNotLogged';
 // eslint-disable-next-line: no-any
 const qrCode = require('qrcode-npm');
 // import uuid from 'uuid'
@@ -8,14 +7,26 @@ type Props = {
   session: string;
 };
 const LoginView = (props: Props) => {
-  let qr = qrCode.qrcode(7, 'M');
+  const qr = qrCode.qrcode(7, 'M');
   qr.addData(props.session);
   qr.make();
-  const qrImage = qr.createImgTag(4);
+  const qrImage: string = qr.createImgTag(4);
   return (
     <div className="index_wrapper">
       <div className="console_background" />
-      <HeaderNotLogged />
+      <header className="wrapper_header">
+        <div className="header_logo" />
+        <div className="header_right">
+          <ul>
+            <li><a>Paper</a></li>
+            <li><a>FAQ</a></li>
+            <li><a>Contact us</a></li>
+            <li><a><span className="icon-twitter" /></a></li>
+            <li><a><span className="icon-medium" /></a></li>
+            <li><a><span className="icon-github" /></a></li>
+          </ul>
+        </div>
+      </header>
       <div className="row">
         <div className="medium-8 columns medium-offset-2">
           <h1 className="qrcode_title">Uniquid Console <span className="title_release">Alpha</span></h1>
@@ -24,7 +35,7 @@ const LoginView = (props: Props) => {
             your digital connected assets inside a network<br />
             of smart devices and people
           </h3>
-          <div className="qrcode_body" dangerouslySetInnerHTML={{ __html: qrImage }} />
+          <div className="qrcode_body" dangerouslySetInnerHTML={ { __html: qrImage } } />
           <p className="qrcode_instruction">Scan the QRcode above with the Uniquid app to Login</p>
           <div className="qrcode_version">v. 0.3.4</div>
         </div>
