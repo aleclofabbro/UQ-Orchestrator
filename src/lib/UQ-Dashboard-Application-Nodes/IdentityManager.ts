ce52ee0ff2f2057eb74d46c2de59d24ba525e073
aleclofabbro <agiansanti@uniquid.com>
2018-02-09 00:26:21 +0100

    better types and polling impl

--- src/lib/UQ-Dashboard-Application-Nodes/IdentityManager.ts
+++ src/lib/UQ-Dashboard-Application-Nodes/IdentityManager.ts
@@ -1,15 +1,22 @@
+import { OrchestratorNode, Node } from './../UQ-Data-Types/index';
 import { Observable } from '@reactivex/rxjs';
 import { Scheduler } from '@reactivex/rxjs/dist/package/Scheduler';
 import { Api, OrchestrateRequest } from 'lib/UQ-IO-Types/Imprinter';
 import { PartialObserver } from '@reactivex/rxjs/dist/package/Observer';
+import { Wallet } from 'lib/UQ-Data-Types';
 
-type Config = {
+interface Config {
   api$: Observable<Api>;
   getNodesReq$: Observable<void>;
   orchestrateReq$: Observable<OrchestrateRequest>;
   $orchestrateRes: PartialObserver<OrchestrateRequest>;
   scheduler?: Scheduler;
   interval: number;
+}
+interface State {
+  nodes: Node[];
+  nodeInfo: Wallet | null;
+  orchestrators: OrchestratorNode[];
 }
 export const IdentityManager = ({
   scheduler,
@@ -18,22 +25,23 @@ export const IdentityManager = ({
   orchestrateReq$,
   $orchestrateRes,
   interval = 10000
-}: Config) => {
+}: Config): Observable<State> => {
   const nodes$ = api$.mergeMap(
     api => getNodesReq$
       .startWith(null)
-      .switchMapTo(Observable.interval(interval, scheduler).startWith(-1))
-      .mergeMap(api.getNodes)
+      .mergeMap(() => api.getNodes()
+        .repeatWhen(notifications => notifications.delay(interval, scheduler))
+      )
   );
 
   const nodeInfo$ = api$.mergeMap(
-    api => Observable.interval(interval, scheduler)
-      .mergeMap(api.getNodeInfo)
+    api => api.getNodeInfo()
+      .repeatWhen(notifications => notifications.delay(interval, scheduler))
   );
 
   const orchestrators$ = api$.mergeMap(
-    api => Observable.interval(interval, scheduler)
-      .mergeMap(api.getOrchestrators)
+    api => api.getOrchestrators()
+      .repeatWhen(notifications => notifications.delay(interval, scheduler))
   );
 
   const orchestrateQueue$ = api$.mergeMap(
